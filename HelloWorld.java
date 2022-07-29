7f1c6c175d84f2b3c7fa6f2e09135656544efb71
Harshali <harshalisingh72@gmail.com>
2022-07-29 15:37:58 +0530

    Java File Commit

---
+++ HelloWorld.java
@@ -0,0 +1,35 @@
+
+/*
+ * * * *
+ *     *
+ *     *
+ * * * *
+ */
+public class HallowSquare {
+
+	public static void main(String[] args)
+	{
+		int i;
+		int j;
+
+		for (i=1;i<=4;i++)
+		{
+			for (j=1; j<=4;j++)
+			{
+				if ((i==1 || i == 4) || (j==1 || j==4))
+				{
+					System.out.print("* ");
+				}
+
+				else
+				{
+					System.out.print("  ");
+				}
+
+
+			}
+
+
+			System.out.println();
+		}
+	}
